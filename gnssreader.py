c8b5ba097bce948d457e87252b0bf2689630ab8c
zsiki <siki1958@gmail.com>
2020-01-26 23:02:31 +0100

    initial code upload

---
+++ gnssreader.py
@@ -0,0 +1,11 @@
+class GNSSReader(object):
+
+    def __init__(self):
+        pass
+
+    def start(self):
+        pass
+
+    def do(self):
+        """ Read position and add to queue in infinite loop"""
+        pass
---
+++ i18n/hu.ts
@@ -0,0 +1,58 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="hu_HU" sourcelanguage="en">
+<context>
+    <name>@default</name>
+    <message>
+        <location filename="../test/test_translations.py" line="48"/>
+        <source>Good morning</source>
+        <translation>Jó reggelt!</translation>
+    </message>
+</context>
+<context>
+    <name>GPRTracker</name>
+    <message>
+        <location filename="../gpr_tracker.py" line="192"/>
+        <source>&amp;gpr_tracker</source>
+        <translation>&amp;grp_tracker</translation>
+    </message>
+    <message>
+        <location filename="../gpr_tracker.py" line="168"/>
+        <source>GPR Tracker</source>
+        <translation>GPR Tracker</translation>
+    </message>
+</context>
+<context>
+    <name>GPRTrackerDockWidgetBase</name>
+    <message>
+        <location filename="../gpr_tracker_dockwidget_base.ui" line="20"/>
+        <source>gpr_tracker</source>
+        <translation>grp_tracker</translation>
+    </message>
+    <message>
+        <location filename="../gpr_tracker_dockwidget_base.ui" line="44"/>
+        <source>Track color</source>
+        <translation>Track szín</translation>
+    </message>
+    <message>
+        <location filename="../gpr_tracker_dockwidget_base.ui" line="51"/>
+        <source>Position:</source>
+        <translation>Pozíció:</translation>
+    </message>
+    <message>
+        <location filename="../gpr_tracker_dockwidget_base.ui" line="58"/>
+        <source>Tracking</source>
+        <translation>NyoNyomkövetés</translation>
+    </message>
+    <message>
+        <location filename="../gpr_tracker_dockwidget_base.ui" line="34"/>
+        <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;Point layer for tracking&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
+        <translation>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;Pont réteg a nyomkövetéshez&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</translation>
+    </message>
+    <message>
+        <location filename="../gpr_tracker_dockwidget_base.ui" line="41"/>
+        <source>Color for layer features</source>
+        <translation>Szín a réteg elemeihez</translation>
+    </message>
+</context>
+</TS>
